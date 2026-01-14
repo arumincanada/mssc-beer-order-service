9b2f69a4e0835684adf5fdfb9c2f7670e1bcf168
arumincanada <bala.sangar@gmail.com>
2026-01-14 14:04:14 -0500

    refactor model

---
+++ src/main/java/com/learning/beer/order/service/services/beer/BeerService.java
@@ -0,0 +1,11 @@
+package com.learning.beer.order.service.services.beer;
+
+import com.learning.brewery.model.BeerDto;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BeerService {
+    Optional<BeerDto> getBeerById(UUID uuid);
+    Optional<BeerDto> getBeerByUpc(String upc);
+}
---
+++ src/main/java/com/learning/beer/order/service/web/mappers/BeerOrderLineMapper.java
@@ -0,0 +1,14 @@
+package com.learning.beer.order.service.web.mappers;
+
+import com.learning.beer.order.service.domain.BeerOrderLine;
+import com.learning.brewery.model.BeerOrderLineDto;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerOrderLineMapperDecorator.class)
+public interface BeerOrderLineMapper {
+    BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
+
+    BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto);
+}
